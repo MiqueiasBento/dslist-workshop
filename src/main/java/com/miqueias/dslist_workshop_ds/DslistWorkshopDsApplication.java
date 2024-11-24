fdfa6381b3aea6c673a7bcfc6c8ee563f7bc7498
Miqueias Bento <miqueias.bento32@gmail.com>
2024-11-24 15:58:12 -0300

    first commit

---
+++ src/main/java/com/miqueias/dslist_workshop_ds/DslistWorkshopDsApplication.java
@@ -0,0 +1,13 @@
+package com.miqueias.dslist_workshop_ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistWorkshopDsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistWorkshopDsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miqueias/dslist_workshop_ds/DslistWorkshopDsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miqueias.dslist_workshop_ds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistWorkshopDsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
