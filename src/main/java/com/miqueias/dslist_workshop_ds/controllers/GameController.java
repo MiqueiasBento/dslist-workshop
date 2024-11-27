946e078389240faa526ddfb1093252b84bc25394
Miqueias Bento <miqueias.bento32@gmail.com>
2024-11-26 23:11:24 -0300

    GET Game by id

--- src/main/java/com/miqueias/dslist_workshop_ds/controllers/GameController.java
+++ src/main/java/com/miqueias/dslist_workshop_ds/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.miqueias.dslist_workshop_ds.controllers;
 
+import com.miqueias.dslist_workshop_ds.entitiesDTO.GameDTO;
 import com.miqueias.dslist_workshop_ds.entitiesDTO.GameMinDTO;
 import com.miqueias.dslist_workshop_ds.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,12 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> findAll() {
         List<GameMinDTO> result = gameService.findAll();
+        return result;
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        GameDTO result = gameService.findById(id);
         return result;
     }
 }
--- src/main/java/com/miqueias/dslist_workshop_ds/services/GameService.java
+++ src/main/java/com/miqueias/dslist_workshop_ds/services/GameService.java
@@ -1,10 +1,12 @@
 package com.miqueias.dslist_workshop_ds.services;
 
 import com.miqueias.dslist_workshop_ds.entities.Game;
+import com.miqueias.dslist_workshop_ds.entitiesDTO.GameDTO;
 import com.miqueias.dslist_workshop_ds.entitiesDTO.GameMinDTO;
 import com.miqueias.dslist_workshop_ds.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,5 +19,11 @@ public class GameService {
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(GameMinDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true) // Nao precisa bloquear o BD para escrit, deixando mais rápido
+    public GameDTO findById(Long id) {
+        Game game = gameRepository.findById(id).get();    // Deve ter um tratamento de excecao pois retorna um optional
+        return new GameDTO(game);
     }
 }
