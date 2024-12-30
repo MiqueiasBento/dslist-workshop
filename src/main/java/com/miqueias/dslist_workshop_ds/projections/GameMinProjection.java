1585b30abb3aa74482cabcf01dd4bf07a29658f5
Miqueias Bento <miqueias.bento.da.silva@gmail.com>
2024-12-30 13:50:16 -0300

    Homologacao do sistema

--- src/main/java/com/miqueias/dslist_workshop_ds/projections/GameMinProjection.java
+++ src/main/java/com/miqueias/dslist_workshop_ds/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.miqueias.dslist_workshop_ds.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
